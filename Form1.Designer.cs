5848ab33e52409bcec22067672df78508e17d76d
Iron/Archivist <88783376+Iron-Archivist@users.noreply.github.com>
2024-02-15 16:55:13 -0500

    Basic Fillable Table

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            GameWindow = new TableLayoutPanel();
+            button1 = new Button();
+            DifficultySelect = new ListBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(GameWindow);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(886, 893);
+            panel1.TabIndex = 0;
+            // 
+            // GameWindow
+            // 
+            GameWindow.AutoSize = true;
+            GameWindow.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            GameWindow.ColumnCount = 2;
+            GameWindow.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            GameWindow.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            GameWindow.Dock = DockStyle.Fill;
+            GameWindow.Location = new Point(0, 0);
+            GameWindow.Name = "GameWindow";
+            GameWindow.RowCount = 2;
+            GameWindow.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            GameWindow.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            GameWindow.Size = new Size(884, 891);
+            GameWindow.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button1.Location = new Point(12, 1033);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // DifficultySelect
+            // 
+            DifficultySelect.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            DifficultySelect.FormattingEnabled = true;
+            DifficultySelect.ItemHeight = 15;
+            DifficultySelect.Items.AddRange(new object[] { "Easy", "Medium", "Hard" });
+            DifficultySelect.Location = new Point(12, 933);
+            DifficultySelect.Name = "DifficultySelect";
+            DifficultySelect.Size = new Size(120, 94);
+            DifficultySelect.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(910, 1079);
+            Controls.Add(DifficultySelect);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "`";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private TableLayoutPanel GameWindow;
+        private Button button1;
+        private ListBox DifficultySelect;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+
 namespace Matching_Card_Game_Project
 {
     public partial class Form1 : Form
@@ -5,6 +8,62 @@ namespace Matching_Card_Game_Project
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int BaseDifficultyValue = 2;
+            int DifficultyValue = BaseDifficultyValue * DifficultySelect.SelectedIndex;
+            int GameColumnCount = (DifficultyValue + 4);
+            int GameRowCount = (DifficultyValue + 2);
+            int GameCellCount = GameColumnCount * GameRowCount;
+            int CurrentCellCount = 0;
+            _ = (0, 0);
+            Label[] TestLabels = new Label[GameCellCount];
+
+            GameWindow.ColumnCount = GameColumnCount;
+            GameWindow.RowCount = GameRowCount;
+
+            for (int i = 0; i <= GameWindow.ColumnCount; i++)
+            {
+                GameWindow.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50)); // Each column occupies 25% of the available space
+            }
+
+            for (int i = 0; i <= GameWindow.RowCount; i++)
+            {
+                GameWindow.RowStyles.Add(new RowStyle(SizeType.Percent, 50)); // Each row occupies 50% of the available space
+            }
+
+            for (int x = 0; x < GameWindow.ColumnCount; x++)
+            {
+                for (int y = 0; y < GameWindow.RowCount; y++)
+                {
+                    Control CurrentCell = GameWindow.GetControlFromPosition(x, y);
+                    if (CurrentCell == null)
+                    {
+                        TestLabels[CurrentCellCount] = new Label();
+                        TestLabels[CurrentCellCount].Text = (CurrentCellCount + 1).ToString();
+                        TestLabels[CurrentCellCount].Dock = DockStyle.Fill;
+                        GameWindow.Controls.Add(TestLabels[CurrentCellCount],x,y);
+                    }
+                    CurrentCellCount++;
+                }
+                
+            }
+            CurrentCellCount = 0;
+
+
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
