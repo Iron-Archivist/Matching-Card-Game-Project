b6be7aef5e3fcc20d93b0688261fe073c620c3ad
Iron/Archivist <88783376+Iron-Archivist@users.noreply.github.com>
2024-02-23 22:21:58 -0500

    Fully Implemented Card Matching, Added Card Art, Victory Condition

--- GameWindow.cs
+++ GameWindow.cs
@@ -19,8 +19,11 @@ namespace Matching_Card_Game
         int GameRowCount;
         int GameCellCount;
         int CurrentCellCount = 0;
+        int Matches = 0;
         Label LastObjectClicked;
-        string LastValueClicked;
+        int LastObjectClickedPosition;
+        PictureBox[] TestPictures;
+        Label[] TestLabels;
 
         public GameWindow()
         {
@@ -33,18 +36,27 @@ namespace Matching_Card_Game
                 GameColumnCount = (DifficultyValue + 4);
                 GameRowCount = (DifficultyValue + 2);
                 GameCellCount = GameColumnCount * GameRowCount;
-                Label[] TestLabels = new Label[GameCellCount];
+                TestLabels = new Label[GameCellCount];
+                TestPictures = new PictureBox[GameCellCount];
+                var GameSet = (TestLabels, TestPictures);
+                GameSet.TestLabels = TestLabels;
+                GameSet.TestPictures = TestPictures;
 
                 GameTable.ColumnCount = GameColumnCount;
                 GameTable.RowCount = GameRowCount;
 
-                TestLabels = CreateGame(TestLabels);
-                FillCells(TestLabels);
+                GameSet = CreateGame(GameSet);
+                TestLabels = GameSet.TestLabels;
+                TestPictures = GameSet.TestPictures;
+                FillCells(TestLabels, TestPictures);
                 ResizeCells();
         }
 
-        private Label[] CreateGame(Label[] GameCells)
+        private (Label[], PictureBox[]) CreateGame((Label[] GameCells, PictureBox[] GameImages)GameSet)
         {
+            Label[] GameCells = GameSet.GameCells;
+            PictureBox[] GameImages = GameSet.GameImages;
+
             int[] RandomNumbers = new int[GameCells.Length];
             int i = 0;
             int j = 0;
@@ -67,18 +79,28 @@ namespace Matching_Card_Game
                     k = i;
                 }
                 GameCells[i] = new Label();
+                GameImages[i] = new PictureBox();
                 GameCells[i].Text = (k + 1).ToString();
+                GameCells[i].Visible = false;
+                GameImages[i].Image = (Image)Properties.Resources.ResourceManager.GetObject("CardBack");
                 GameCells[i].Dock = DockStyle.Fill;
+                GameImages[i].Dock = DockStyle.Fill;
+                GameImages[i].SizeMode = PictureBoxSizeMode.StretchImage;
                 GameCells[i].Tag = RandomNumbers[i];
+                GameImages[i].Tag = RandomNumbers[i];
                 GameCells[i].Click += GameObjectClick;
+                GameImages[i].Click += GameImageClick;
             }
 
             Label[] RandomizedLabels = GameCells.OrderBy(Label => Label.Tag).ToArray();
+            PictureBox[] RandomizedImages = GameImages.OrderBy(Label => Label.Tag).ToArray();
+            GameSet.GameCells = RandomizedLabels;
+            GameSet.GameImages = RandomizedImages;
 
-            return RandomizedLabels;
+            return GameSet;
         }
-        private void FillCells(Label[] Labels)
-        {
+        private void FillCells(Label[] Labels, PictureBox[] Pictures)
+        {         
             for (int x = 0; x < GameTable.ColumnCount; x++)
             {
                 for (int y = 0; y < GameTable.RowCount; y++)
@@ -86,8 +108,8 @@ namespace Matching_Card_Game
                     Control CurrentCell = GameTable.GetControlFromPosition(x, y);
                     if (CurrentCell == null)
                     {
-
-                        GameTable.Controls.Add(Labels[CurrentCellCount], x, y);
+                       GameTable.Controls.Add(Pictures[CurrentCellCount], x, y);
+                       GameTable.Controls.Add(Labels[CurrentCellCount], x, y);
                     }
                     CurrentCellCount++;
                 }
@@ -99,45 +121,62 @@ namespace Matching_Card_Game
         {
             for (int i = 0; i < GameTable.ColumnCount; i++)
             {
-                GameTable.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50)); // Each column occupies 25% of the available space
+                GameTable.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
             }
 
             for (int i = 0; i < GameTable.RowCount; i++)
             {
-                GameTable.RowStyles.Add(new RowStyle(SizeType.Percent, 50)); // Each row occupies 50% of the available space
+                GameTable.RowStyles.Add(new RowStyle(SizeType.Percent, 50));
             }
         }
         private void GameObjectClick(object sender, EventArgs e)
         {
+            int ClickedArrayPosition = Array.IndexOf(TestLabels, sender);
             Label ClickedObject = (Label)sender;
+            TestPictures[ClickedArrayPosition].Image = (Image)Properties.Resources.ResourceManager.GetObject("Card" + (ClickedObject.Text));
+
             if (LastObjectClicked != null)
             {
                 if (ClickedObject == LastObjectClicked)
                 {
                     MessageBox.Show("same thing clicked twice");
                     LastObjectClicked = null;
-                    LastValueClicked = null;
                 }
                 else if (ClickedObject.Text == LastObjectClicked.Text)
                 {
-                    MessageBox.Show("Correct Match.");
+                    Matches = Matches + 1;
+                    MessageBox.Show("Correct Match." + Matches.ToString() + "/ " + GameCellCount.ToString());
+                    TestPictures[ClickedArrayPosition].Visible = false;
+                    TestPictures[LastObjectClickedPosition].Visible = false;
                     LastObjectClicked = null;
-                    LastValueClicked = null;
+                    
+                    if (Matches >= (GameCellCount/2))
+                    {
+                        MessageBox.Show("Victory");
+                    }
+
+
                 }
                 else
                 {
                     MessageBox.Show("No Match");
+                    TestPictures[ClickedArrayPosition].Image = (Image)Properties.Resources.ResourceManager.GetObject("CardBack");
+                    TestPictures[LastObjectClickedPosition].Image = (Image)Properties.Resources.ResourceManager.GetObject("CardBack");
                     LastObjectClicked = null;
-                    LastValueClicked = null;
                 }
             }
             else
             {
                 LastObjectClicked = ClickedObject;
-                LastValueClicked = ClickedObject.Text;
+                LastObjectClickedPosition = ClickedArrayPosition;
             }
 
 
+        }
+        private void GameImageClick(object sender, EventArgs e)
+        {
+            int ArrayPosition = Array.IndexOf(TestPictures, sender);
+            GameObjectClick(TestLabels[ArrayPosition], EventArgs.Empty);
         }
 
     }
